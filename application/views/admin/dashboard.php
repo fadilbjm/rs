da2771f877695228e9b27df3383e31fe2cb00dc8
Noor Fadhila Fadhal Ahmad <fadilby89@gmail.com>
2019-03-02 15:06:52 +0800

    highchart is sucks!!!!

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,8 +1,8 @@
 <div class="container">
     <div class="text-center">
         <div class="row">
-            <div class="col-lg-12" style="border-radius: 5px; padding:10px;">
-                <div id="char" style="width:400px; height:50px;"></div>
+            <div class="col-lg-6" style="border-radius: 5px; padding:10px;">
+                <div id="char" style="width:800px; height:500px;"></div>
                 
             </div>
         </div>
@@ -23,18 +23,75 @@
     <script src="/rs/aset/js/highcharts.js"></script>
 	<script type="text/javascript">
 		$(document).ready(function (){
-
-
-
-        });
+            
+            Highcharts.chart('char',{
+                chart: {
+        type: 'bar'
+    },
+    title: {
+        text: 'Historic World Population by Region'
+    },
+    subtitle: {
+        text: 'Source: <a href="https://en.wikipedia.org/wiki/World_population">Wikipedia.org</a>'
+    },
+    xAxis: {
+        categories: ['Africa', 'America', 'Asia', 'Europe', 'Oceania'],
+        title: {
+            text: null
+        }
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Population (millions)',
+            align: 'high'
+        },
+        labels: {
+            overflow: 'justify'
+        }
+    },
+    tooltip: {
+        valueSuffix: ' millions'
+    },
+    plotOptions: {
+        bar: {
+            dataLabels: {
+                enabled: true
+            }
+        }
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'top',
+        x: -40,
+        y: 80,
+        floating: true,
+        borderWidth: 1,
+        backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+        shadow: true
+    },
+    credits: {
+        enabled: false
+    },
+    series: [{
+        name: 'Year 1800',
+        data: [107, 31, 635, 203, 2]
+    }, {
+        name: 'Year 1900',
+        data: [133, 156, 947, 408, 6]
+    }, {
+        name: 'Year 2000',
+        data: [814, 841, 3714, 727, 31]
+    }, {
+        name: 'Year 2016',
+        data: [1216, 1001, 4436, 738, 40]
+    }]
+            });
         
-        function penyakit() {
-            
-        }
 
-        function pasien() {
-            
-        }
-	</script>
+        
+    });
+        </script>
   </body>
 </html>
